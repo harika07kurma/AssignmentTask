e5b38a4174579032ea944952c89bdfe27b884e7d
harika07kurma <78656512+harika07kurma@users.noreply.github.com>
2021-02-06 19:33:39 -0600

    Lazy loading implementation
    Implemented lazy loading and by default redirectiing to git rpos list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  
+  {
+    path: '',
+    redirectTo: '/git-repo-list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'git-repo-list',
+    loadChildren: () => import('./_views/git-repos/git-repos.module').then(m => m.GitReposModule)
+  }
 ];
 
 @NgModule({
